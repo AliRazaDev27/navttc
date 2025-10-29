fb3ce5e9042cacb616b83ff76af6acc2c453c1ae
Ali Raza <alirazadev27@gmail.com>
2025-10-29 12:02:01 +0500

    Product grid with cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { useEffect, useState } from 'react';
 import ProductCard from './components/product-card';
 export default function App() {
-  const [data,setData] = useState(null);
+  const [data,setData] = useState([]);
+  console.log(data);
   useEffect(() => {
-    fetch('https://dummyjson.com/products/1')
+    fetch('https://dummyjson.com/products')
     .then(res => res.json())
-    .then(json => setData(json));
+    .then(json => setData(json.products));
   },[]);
   return (
-    <div>
-      <ProductCard product={data!}/>
+    <div className='grid'>
+      {
+        data && data.map((item,index) => <ProductCard key={index} product={item} />)
+      }
     </div>
   );
 }
