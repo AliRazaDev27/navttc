6cd55009e9b5da401ae1f65a0e47fa35cf0dc31e
Ali Raza <alirazadev27@gmail.com>
2025-10-30 12:39:05 +0500

    Installed React Router

--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -14,9 +14,9 @@ export default function ProductCard({ product }: { product: Product }) {
                 <img src={product.thumbnail} alt={product.title} />
             </div>
             <div className="p-4">
-                <h2 className="text-xl font-bold m-0">{product.title}</h2>
+                <h2 className="text-xl font-bold m-0 line-clamp-1">{product.title}</h2>
                 <p className="text-sm text-[#666] mx-0 my-1">{product.brand}</p>
-                <p className="text-sm mx-0 my-2">{product.description}</p>
+                <p className="text-sm mx-0 my-2 line-clamp-3">{product.description}</p>
                 <div className="flex items-center mx-0 my-2">
                     <span className="text-[1.2rem] font-bold text-[#e91e63]">${discountedPrice}</span>
                     <span className="text-[[0.9rem] text-[#635f5f] line-through ml-2">${product.price.toFixed(2)}</span>
@@ -27,8 +27,9 @@ export default function ProductCard({ product }: { product: Product }) {
                 </div>
             </div>
             <div className='mt-auto'>
-                <button className='bg-red-500 w-full p-2.5 text-white rounded-lg cursor-pointer text-base'>
-                    Add to Cart</button>
+                <button className='bg-blue-600 hover:bg-blue-700 w-full p-2.5 text-white rounded-lg cursor-pointer text-base'>
+                Details
+                </button>
             </div>
         </div>
     );
