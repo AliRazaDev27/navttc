a8f0723f5b77c5baf10173e0dbbe13821a1662b5
UrwaByte <urwasultan9@gmail.com>
2025-11-14 01:57:31 -0800

    test

--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router"
 export default function Homepage(){
   return(
     <main>
+      <h1>test</h1>
       <section className="w-full md:bg-[url('/src/assets/hero-desktop.png')] bg-contain bg-right bg-no-repeat  bg-[#F2F0F1]">
         <div className="md:ms-20 px-4 md:px-0 py-8 md:py-16  flex flex-col justify-center gap-3 md:gap-4">
           <div className="text-4xl md:text-6xl font-bold">
